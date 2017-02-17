47e9452b28d537bee0aabaa6690d47c93ec8a482
Alejandro Alcalde <algui91@gmail.com>
2017-02-17 22:29:37 +0100

    Added test for two observations

--- Examples/src/main/scala/elbauldelprogramador/MyNaiveBayesWrapper.scala
+++ Examples/src/main/scala/elbauldelprogramador/MyNaiveBayesWrapper.scala
@@ -1,6 +1,26 @@
 package elbauldelprogramador.naivebayes
 
-case class MyNaiveBayesWrapper(classification:Option[String] = None, observations:Double) {
-  def train: Option[String] = classification
-  def classify(observation:Double): Option[String] = classification
+import scala.language.postfixOps
+
+import org.log4s._
+
+case class MyNaiveBayesWrapper(y:Option[Seq[String]] = None,
+  observations: Seq[Double] = Seq.empty) {
+
+  private[this] val log = getLogger
+
+  private[this] val model = y match {
+    case Some(x) => x zip observations toMap
+    case None => Map.empty[String, Double]
+  }
+
+  def classify(observation:Double): Option[String] = model match {
+    case label if model.nonEmpty =>
+      val closest = label.values.minBy(v => math.abs(observation - v))
+      log.warn(s"$model")
+      log.warn(s"CLOSEST: $closest")
+      log.warn(s"find: ${model.find(_._2 == closest)}")
+      Some(model.find(_._2 == closest).get._1)
+    case _ => None
+  }
 }
--- Examples/src/test/scala/elbauldelprogramador/NaiveBayesSpec.scala
+++ Examples/src/test/scala/elbauldelprogramador/NaiveBayesSpec.scala
@@ -21,9 +21,16 @@ class NaiveBayesSpec extends Specification
         Given an observation for a class A with an observation 0
         When classifying an observation 23.2
         Then the classifier should classify the observation as A ${oneObservationSce.end}
+
+                                                                 ${twoObservationsSce.start}
+        Given the observation 23.2
+        Given another observation 73.2
+        When Classifying
+        Then 23.2 should classify as A class and 72.3 as B       ${twoObservationsSce.end}
     """
 
-  val myD = groupAs("\\d+\\.\\d+").and((s: String) => s.toDouble)
+  val myDouble = groupAs("\\d+\\.\\d+").and((s: String) => s.toDouble)
+  val myTwoDoubles = groupAs("\\d+\\.\\d+").and((s: String, ss: String) => (s.toDouble, ss.toDouble))
 
   private[this] val logger = getLogger
 
@@ -35,8 +42,7 @@ class NaiveBayesSpec extends Specification
 
   private[this] val noObservationSce = Scenario("NoObs").
     when(){ case _ =>
-
-      val classifier = MyNaiveBayesWrapper(observations = 23.2)
+      val classifier = MyNaiveBayesWrapper(observations = Vector(23.2))
       classifier.classify(23.2)
     }.
     andThen(){ case _ :: result :: _ =>
@@ -45,13 +51,27 @@ class NaiveBayesSpec extends Specification
 
   private[this] val oneObservationSce = Scenario("oneObs").
     given(anInt).
-    when(myD){ case obs :: a :: _ =>
-      val classifier = MyNaiveBayesWrapper(Some("a"), obs)
-      classifier.train
+    when(myDouble){ case obs :: a :: _ =>
+      val classifier = MyNaiveBayesWrapper(Some(Vector("a")), Vector(obs))
       classifier.classify(obs)
     }.
     andThen(){ case _ :: result :: _ =>
       result must_== Some("a")
+    }
+
+  private[this] val twoObservationsSce = Scenario("twoObs").
+    given(myDouble).
+    given(myDouble).
+    when() {case _ :: obs1 :: obs2 :: _ =>
+      val classifier = MyNaiveBayesWrapper(Some(Vector("a class", "b class")),
+        observations = Vector(0.0, 100.0))
+      val classification = classifier.classify(obs1)
+      val classification2 = classifier.classify(obs2)
+
+      (classification, classification2)
+    }.
+    andThen(){ case _ :: r :: _ =>
+      (r._1.get must_== "b class") and (r._2.get must_== "a class")
     }
 }
 
